52ec734e2e85a28a35d28cf654123e7eafad8864
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2025-05-31 11:54:32 +0200

    feat: add drizzle for managing db

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import fastify, { FastifyInstance } from "fastify";
+import dbPlugin from "@/plugins/db";
 
 function build(options = {}): FastifyInstance {
   const app = fastify(options);
 
+  app.register(dbPlugin, { connectionString: process.env.DATABASE_URL! });
+
   app.get("/", async (request, reply) => {
     return { hello: "world!" };
+  });
+
+  app.get("/ping", async (request, reply) => {
+    const result = await app.db.execute("SELECT NOW()");
+    return { result: result.rows[0] };
   });
 
   return app;
---
+++ src/plugins/db.ts
@@ -0,0 +1,27 @@
+import { drizzle, type NodePgDatabase } from "drizzle-orm/node-postgres";
+import fastifyPlugin from "fastify-plugin";
+import { Pool } from "pg";
+import type { FastifyInstance, FastifyPluginOptions } from "fastify";
+
+export interface DbPluginOptions extends FastifyPluginOptions {
+  connectionString: string;
+}
+
+declare module "fastify" {
+  interface FastifyInstance {
+    db: NodePgDatabase;
+  }
+}
+
+const dbPlugin = async (fastify: FastifyInstance, options: DbPluginOptions) => {
+  const pool = new Pool({ connectionString: options.connectionString });
+  const db = drizzle(pool);
+
+  fastify.decorate("db", db);
+
+  fastify.addHook("onClose", async () => {
+    await pool.end();
+  });
+};
+
+export default fastifyPlugin<DbPluginOptions>(dbPlugin);

abefb525736b986cbe8504a806bede3330fa3cc7
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2025-07-22 21:54:36 +0200

    feat: add adventure types seeding

---
+++ drizzle/0002_seed-adventure-types.sql
@@ -0,0 +1,4 @@
+-- Custom SQL migration file, put your code below! --
+INSERT INTO "adventure_types" ("description") VALUES('Fantasy');
+INSERT INTO "adventure_types" ("description") VALUES('Science Fiction');
+INSERT INTO "adventure_types" ("description") VALUES('Contemporary Thriller');

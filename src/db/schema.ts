53f61acd2c70ac4c027e8d797db41296cdf8431a
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2025-07-27 07:56:04 +0200

    feat: add chapters and chaper_choices tables

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { boolean, pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
+import { boolean, pgTable, text, timestamp, uuid, smallint } from 'drizzle-orm/pg-core';
 
 export const adventuresTable = pgTable('adventures', {
   id: uuid().primaryKey().defaultRandom(),
@@ -13,4 +13,23 @@ export const adventuresTable = pgTable('adventures', {
 export const adventureTypesTable = pgTable('adventure_types', {
   id: uuid().primaryKey().defaultRandom(),
   description: text(),
+});
+
+export const chaptersTable = pgTable('chapters', {
+  id: uuid().primaryKey().defaultRandom(),
+  number: smallint().notNull(),
+  story_so_far: text().notNull(),
+  created: timestamp().notNull(),
+  adventure_id: uuid()
+    .notNull()
+    .references(() => adventuresTable.id),
+});
+
+export const chapterChoicesTable = pgTable('chapter_choices', {
+  id: uuid().primaryKey().defaultRandom(),
+  action: text().notNull(),
+  chosen: boolean().default(false),
+  chapter_id: uuid()
+    .notNull()
+    .references(() => chaptersTable.id),
 });

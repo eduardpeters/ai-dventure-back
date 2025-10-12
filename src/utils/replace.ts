a07b4febf6c1e655bb39a2c9e05a86c2764fff66
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2025-10-12 09:14:57 +0200

    types: constrain Placeholder strings

--- src/utils/replace.ts
+++ src/utils/replace.ts
@@ -1,12 +1,16 @@
-type ReplacementMapping = Record<string, string>;
+type Placeholder = `%${string}%`;
+type ReplacementMap = Record<Placeholder, string>;
 
 /**
  * Takes a template text with specified placeholders and replaces them with the provided replacements.
  * Placeholders use % as delimiter: %textToReplace%
  * The replacements are a map with keys being the placeholders and values the text to replace them.
  */
-export default function replace(templateText: string, replacements: ReplacementMapping): string {
+export default function replace(templateText: string, replacements: ReplacementMap): string {
+  if (Object.keys(replacements).length === 0) {
+    return templateText;
+  }
   return templateText.replace(/%\w+%/g, function (match: string) {
-    return replacements[match] || match;
+    return replacements[match as Placeholder] || match;
   });
 }
